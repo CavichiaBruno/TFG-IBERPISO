bd8945c97d9402657b0c56264d03c4395d6ccf1c
Bruno Cavichia <cavichiabruno@gmail.com>
2026-05-10 19:14:09 +0200

    ci/cd: fix bootstrap/app.php syntax

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,9 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->afterResolving(Application::class, function ($app) {
-        if (isset($_SERVER['VERCEL_URL'])) {
-            $app->useBootstrapPath('/tmp');
-        }
-    })
     ->create();
+
+if (isset($_SERVER['VERCEL_URL'])) {
+    $app->useBootstrapPath('/tmp');
+}
+
+return $app;
